e5642e204d6b670688b2d1f59a6f66daf14befd9
souru98 <kulkarnisourabh1998@gmail.com>
2020-03-21 21:32:40 +0530

    my code

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,33 @@
+package controller;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+import dao.SkillDAO;
+import model.Skill;
+
+public class Main
+{
+	public static void main(String args[])throws IOException, ClassNotFoundException, SQLException
+	{
+		SkillDAO skilldao = new SkillDAO();
+		
+		TreeMap<Integer,Skill> map = new TreeMap<Integer,Skill>();
+		
+		map=skilldao.skillCount();
+		System.out.println("Id"+"\t"+"name  "+"\t"+"\t"+"count");
+		
+		for(Entry<Integer,Skill> pair: map.entrySet())
+		{
+			int  count=pair.getKey();
+			Skill skill=pair.getValue();
+			System.out.println(skill.getSkilld()+"\t"+skill.getSkillName()  +"\t"+"\t"+count);
+			
+		}
+		
+	
+	}
+	
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,72 @@
 
+package dao;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.TreeMap;
+import java.util.Map.Entry;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO
+{
+	public TreeMap<Integer,Skill> skillCount() throws ClassNotFoundException, SQLException, IOException 
+	{
+		Connection con=ConnectionManager.getConnection();
+		
+		Statement stmt=  con.createStatement();
+		int battingCount =0;
+		int bowlingCount=0;
+		int allrounderCount=0;
+		
+		ResultSet count =stmt.executeQuery("select * from player ");
+		
+		
+		
+		
+		while(count.next())
+		{
+			if(count.getInt(4)==1)
+			{
+				battingCount ++;
+				
+			}
+			else if(count.getInt(4)==2)
+			{
+				bowlingCount ++;
+			}
+			else
+			{
+				allrounderCount ++;
+			}
+		
+		}
+		
+		
+		
+		Skill s1=new Skill((long)123,"batting");
+		Skill s2=new Skill((long)127,"bowling");
+		Skill s3=new Skill((long)130,"keeping");
+		
+		TreeMap<Integer,Skill> map=new TreeMap<Integer,Skill>();
+		
+		Integer i1=new Integer(battingCount);
+		Integer i2=new Integer(bowlingCount);
+		Integer i3=new Integer(allrounderCount);
+		
+		map.put(i1,s1);
+		map.put(i2,s2);
+		map.put(i3,s3);
+		
+		
+		
+		return map;
+	
+	}
+	
+}
+
